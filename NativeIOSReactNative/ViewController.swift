7fb951fa41909d6471c85ca30a429025e1e7ede8
Kamaal <kamaal@tranzer.com>
2021-07-15 08:39:06 +0200

    Initial commit

---
+++ NativeIOSReactNative/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NativeIOSReactNative
+//
+//  Created by Kamaal Farah on 15/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+}
